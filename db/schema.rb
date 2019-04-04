81f7f992c37cfc46bf4887bcfba18f0db5652957
schemr <schemryj@gmail.com>
2019-04-04 20:28:03 +0900

    Add tag field, show date

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190404102004) do
+ActiveRecord::Schema.define(version: 20190404111137) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tag"
   end
 
 end
